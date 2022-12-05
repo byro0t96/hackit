ce11388bc0023545faec6e216df45198dd0126b6
byroot96 <63003988+byroot96@users.noreply.github.com>
2022-12-05 05:00:38 +0100

    Create hackit.py

---
+++ hackit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding:utf-8
+from hiphp import *
+
+# connect:
+p1=hiphp(key="123",url="http://127.0.0.1/index.php")#Default: retu=False.
+
+# Get the hole Code:
+p1.get_hole()
+
+# Command line interface:
+p1.cli()
